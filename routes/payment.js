478e63dec41162bb00a61d77416e35595792b5d7
amitkain7 <kainturagolu688@gmail.com>
2023-11-06 20:01:35 +0530

    t

--- routes/payment.js
+++ routes/payment.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 // create Order
 
 router.post('/order',  async (req, res) => {
+   
     try {
 
         const instance = new RazorPay({
@@ -15,11 +16,11 @@ router.post('/order',  async (req, res) => {
         });
 
         const options = {
-            amount: Number(req.body.amount * 100),
+            amount: req.body.amount * 100,
             currency: "INR",
         };
         const order = await instance.orders.create(options);
-
+          
         res.status(200).json({
             success: true,
             data : order,
@@ -37,15 +38,14 @@ router.post('/order',  async (req, res) => {
 // payment verify
 router.post('/verify',  async (req, res) => {
     try {
-
-        const { razorpay_order_id,
-            razorpay_payment_id,
-            razorpay_signature } = req.body
+       
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body
         const expectedSign = crypto
             .createHmac('sha256', process.env.key_secret)
-            .update(razorpay_order_id + '|' + razorpay_payment_id)
+            .update(razorpay_order_id + "|" + razorpay_payment_id)
             .digest('hex')
-
+       
+        
         if (razorpay_signature === expectedSign) {
             res.status(200).json({ msg: 'payment verified successfully' })
         } else {
