6b9862be674cb24469f8e9abae1d30faffccb1b9
Amit <121176159+amitkain7@users.noreply.github.com>
2023-11-06 09:04:20 +0530

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -19,14 +19,23 @@ router.post('/order', verifyToken, async (req, res) => {
             receipt: crypto.randomBytes(10).toString('hex')
         }
 
-        instance.orders.create(options, (err, order) => {
-            if (err) {
-                console.log(err)
-                res.status(500).json({ msg: 'Something went wrong' })
-            }
-            res.status(200).json({ data: order })
+        // instance.orders.create(options, (err, order) => {
+        //     if (err) {
+        //         console.log(err)
+        //         res.status(500).json({ msg: 'Something went wrong' })
+        //     }
+        //     res.status(200).json({ data: order })
 
-        })
+        // })
+
+         const response = await instance.orders.create(options)
+
+        if (response.error) {
+            console.log(res)
+            res.status(500).json({ msg: 'Something went wrong' })
+            return
+        }
+        res.status(200).json({ data: response })
     }
     catch (error) {
         console.log(error)
