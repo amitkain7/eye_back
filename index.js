4d6f4e5871cdfa4699f1e41646dd2348fe030044
Amit <121176159+amitkain7@users.noreply.github.com>
2023-11-06 11:00:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,14 +18,15 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(helmet())
-app.use(cors(
-    {
-        origin : ["https://eyeware-client.vercel.app"],
-        methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        credentials : true
-    }
-    ));
-// app.use(cors())
+// app.use(cors(
+//     {
+//         origin : ["https://eyeware-client.vercel.app"],
+//         methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//         credentials : true
+//     }
+//     ));
+
+app.use(cors())
 
 
 app.get('/', (req, res) => {
